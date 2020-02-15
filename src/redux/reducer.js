1ca0a3a0f426a05365534d5d9cede22090bc49fc
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-02-15 12:32:55 -0700

    adds basic redux outline

---
+++ src/redux/reducer.js
@@ -0,0 +1,23 @@
+const initialState = {
+    homes: []
+}
+
+//Action Types
+
+
+
+// Reducer Function 
+function reducer(state = initialState, action){
+    const {type, payload} = action;
+    switch(type){
+        default:
+             return state
+    }
+}
+
+
+//Action Creators
+
+
+
+export default reducer;
